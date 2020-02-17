65ea3d8f2607bfea80ec4232996113749b5cf5f5
Ezzat Qupty <ezzat.qupty12345@gmail.com>
2020-02-17 13:21:25 +0200

    UI Done

--- Calculator Layout iOS13/ViewController.swift
+++ Calculator Layout iOS13/ViewController.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	@IBOutlet weak var result: UILabel!
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+		result.text = String(10)
     }
 
 
